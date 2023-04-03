b467802a996adca23d3e5c11ffc48b4769194675
Serezha <volos11223344@yandex.ru>
2023-04-03 11:28:53 +0300

    добавляем интеракт карточки на страницу

--- js/app.js
+++ js/app.js
@@ -12,5 +12,58 @@ async function  getMovies(url) {
     }
   });
   const respData = await response.json();
+  showMovies(respData);
   console.log(respData);
 }
+
+function showMovies(data) {
+  const moviesEl = document.querySelector('.movies');
+  data.films.forEach(movie => {
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
+    <div class="movie">
+    <div class="movie__cover-inner">
+      <img class="movie__cover" src="${movie.posterUrlPreview}" alt="${movie.nameRu}">
+      <div class="movie__cover--darkened"></div>
+    </div>
+    <div class="movie__info">
+      <div class="movie__title">${movie.nameRu}</div>
+      <div class="movie__category">${movie.genres.map((genre)=> `${genre.genre}`)}</div>
+      <div class="movie__average movie__average--green">9</div>
+    </div>
+  </div>
+    `
+    moviesEl.appendChild(movieEl);
+  });
+}
+
+// "films": [
+//   {
+//     "filmId": 1267348,
+//     "nameRu": "Джон Уик 4",
+//     "nameEn": "John Wick: Chapter 4",
+//     "year": "2023",
+//     "filmLength": "02:49",
+//     "countries": [
+//       {
+//         "country": "США"
+//       }
+//     ],
+//     "genres": [
+//       {
+//         "genre": "триллер"
+//       },
+//       {
+//         "genre": "криминал"
+//       },
+//       {
+//         "genre": "боевик"
+//       }
+//     ],
+//     "rating": "7.9",
+//     "ratingVoteCount": 25574,
+//     "posterUrl": "https://kinopoiskapiunofficial.tech/images/posters/kp/1267348.jpg",
+//     "posterUrlPreview": "https://kinopoiskapiunofficial.tech/images/posters/kp_small/1267348.jpg",
+//     "ratingChange": null
+//   },
