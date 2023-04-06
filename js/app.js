00332dcb70c037d874ed7c097b668016c198e873
Serezha <volos11223344@yandex.ru>
2023-04-06 10:49:13 +0300

    добавляет закрытия мод окна

--- js/app.js
+++ js/app.js
@@ -34,25 +34,27 @@ function showMovies(data) {
 
   const moviesEl = document.querySelector('.movies');
   data.films.forEach(movie => {
+    console.log(movie);
     const movieEl = document.createElement('div');
     movieEl.classList.add('movie');
     movieEl.innerHTML = `
     <div class="movie">
-    <div class="movie__cover-inner">
-      <img class="movie__cover" src="${movie.posterUrlPreview}" alt="${movie.nameRu}">
-      <div class="movie__cover--darkened"></div>
+      <div class="movie__cover-inner">
+        <img class="movie__cover" src="${movie.posterUrlPreview}" alt="${movie.nameRu}">
+        <div class="movie__cover--darkened"></div>
+      </div>
+      <div class="movie__info">
+        <div class="movie__title">${movie.nameRu}</div>
+        <div class="movie__category">${movie.genres.map((genre)=> ` ${genre.genre}`)}</div>
+        ${
+          movie.rating !== "null" &&
+        `
+        <div class="movie__average movie__average--${getClassByRate(movie.rating)}">${movie.rating}</div>`
+        }
+      </div>
     </div>
-    <div class="movie__info">
-      <div class="movie__title">${movie.nameRu}</div>
-      <div class="movie__category">${movie.genres.map((genre)=> ` ${genre.genre}`)}</div>
-      ${
-        movie.rating !== "null" &&
-      `
-      <div class="movie__average movie__average--${getClassByRate(movie.rating)}">${movie.rating}</div>`
-      }
-    </div>
-  </div>
-    `
+    `;
+    movieEl.addEventListener('click',()=> openModal(movie.filmId));
     moviesEl.appendChild(movieEl);
   });
 }
@@ -74,23 +76,46 @@ form.addEventListener('submit', (e)=> {
 });
 
 //Modal
-const modalElement = document.querySelector('.modal');
+const modalEl = document.querySelector('.modal');
 
+async function openModal(id){
+  console.log(id)
+  modalEl.classList.add('modal--show');
 
-modalEl.innerHTML = `
+  modalEl.innerHTML = `
     <div class="modal__card">
-      <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+      <img class="modal__movie-backdrop" src="" alt="">
       <h2>
-        <span class="modal__movie-title">${respData.nameRu}</span>
-        <span class="modal__movie-release-year"> - ${respData.year}</span>
+        <span class="modal__movie-title">Название</span>
+        <span class="modal__movie-release-year">Год</span>
       </h2>
       <ul class="modal__movie-info">
         <div class="loader"></div>
-        <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
-        ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
-        <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
-        <li class="modal__movie-overview">Описание - ${respData.description}</li>
+        <li class="modal__movie-genre">Жанр</li>
+        <li class="modal__movie-runtime">Время -минут</li>
+        <li class="modal__movie-overview">Описание</li>
       </ul>
       <button type="button" class="modal__button-close">Закрыть</button>
     </div>
-    `;
+  `;
+  const btnClose = document.querySelector(".modal__button-close");
+  console.log(btnClose);
+  btnClose.addEventListener('click',()=> closeModal());
+}
+
+function closeModal() {
+  modalEl.classList.remove('modal--show');
+  console.log('elfkztn');
+}
+
+window.addEventListener('click', (e)=>{
+  if(e.target === modalEl) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', (e)=>{
+  if(e.key ==='Escape') {
+    closeModal();
+  }
+});
